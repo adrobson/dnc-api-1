d1a04d5478b4791c5c998d4cad9319aae5e405cd
Alistair Robson <adrobson@hotmail.com>
2020-04-22 14:42:31 +0100

    Initial
    Initial

---
+++ dnc-api-1/Controllers/FundController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace dnc_api_1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FundController : ControllerBase
+    {
+
+        private readonly ILogger<CountryController> _logger;
+
+        List<Fund> fundList = new List<Fund>();
+
+
+        public FundController()
+        {
+            var countryList = new CountryList();
+
+            var countries = countryList.Countries;
+
+            fundList = new List<Fund>();
+
+            for (int i = 1; i < 6; i++)
+            {
+                Country currentCountry = countries.Where(x => x.CountryId == i).First();
+
+                for (int j = 1; j < 21; j++)
+                {
+                    fundList.Add(new Fund
+
+                    {
+                        CountryId = i,
+
+                        FundId = i * j,
+
+                        FundName = "Fund " + i * j + " - " + currentCountry.CountryName
+
+                    });
+                }
+            }
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Fund> Get(int countryId)
+        {
+            return fundList.Where(x => x.CountryId == countryId);
+        }
+    }
+}
---
+++ dnc-api-1/Controllers/StockController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace dnc_api_1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StockController : ControllerBase
+    {
+
+        private readonly ILogger<CountryController> _logger;
+
+        List<Stock> stockList = new List<Stock>();
+
+
+        public StockController()
+        {
+            var countryList = new CountryList();
+
+            var countries = countryList.Countries;
+
+            stockList = new List<Stock>();
+
+            for (int i = 1; i < 6; i++)
+            {
+                Country currentCountry = countries.Where(x => x.CountryId == i).First();
+
+                for (int j = 1; j < 21; j++)
+                {
+                    stockList.Add(new Stock
+
+                    {
+                        CountryId = i,
+
+                        StockId = i * j,
+
+                        StockName = "Stock " + i * j + " - " + currentCountry.CountryName
+
+                    });
+                }
+            }
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Stock> Get(int countryId)
+        {
+            return stockList.Where(x => x.CountryId == countryId);
+        }
+    }
+}
